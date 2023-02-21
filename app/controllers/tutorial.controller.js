69f0a8c636767aaef1984be5470ea08887139825
madhulisa <azureuser@LearningAzureMadhulisa.nrn1vcdu4pmuzdgvynaqgo5ine.px.internal.cloudapp.net>
2023-02-21 11:43:56 +0000

    CRUD completed

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -42,6 +42,7 @@ exports.findAll = (req, res) => {
 
 // Find a single Tutorial with an id (id as a param)
 exports.findOne = (req, res) => {
+
     const id = req.params.id;
 
     Tutorial.findById(id)
@@ -59,4 +60,95 @@ exports.findOne = (req, res) => {
                     message: err.message || "Some error occurred while fetching the Tutorial with id: ." + id
                 });
             });
-}
+};
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+    
+    if(!req.body) {
+        return res.status(400).send({
+            message: "No ID specified for update!"
+        });
+    }
+
+    const id = req.params.id;
+
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+            .then((data) => {
+                if(!data) {
+                    res.status(400).send({
+                        message: "No data found to update aggainst id: " + id
+                    });
+                } else {
+                    res.send({
+                        message: "Data supdated successfully!"
+                    });
+                }
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || "Error updating tutorial with id: " + id
+                });
+            });
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+
+    if(!req.body) {
+        return res.status(400).send({
+            message: "No Id found to delete tutorial"
+        });
+    }
+
+    const id = req.params.id;
+
+    Tutorial.findByIdAndRemove(id)
+            .then((data) => {
+                if(!data) {
+                    res.status(400).send({
+                        message: "No tutorial found with the id: " + id
+                    });
+                } else {
+                    res.send({
+                        message: "Tutorial deleted succesfully with id: " + id
+                    });
+                }
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || "Error deleting Tutorial with id: ." + id
+                });
+            })
+};
+
+// Delete all Tutorials from the database
+exports.deleteAll = (req, res) => {
+
+    Tutorial.deleteMany({})
+            .then((data) => {
+                res.send({
+                    message: `${data.deletedCount} Tutorials were deleted successfully!`
+                });
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while removing all tutorials."
+                });
+            })
+};
+
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+    Tutorial.find({ published: true})
+            .then((data) => {
+                console.log(data)
+                res.send(data);
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while retriving published tutorials."
+                });
+            })
+};
--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -11,7 +11,15 @@ module.exports = app => {
 
     router.get("/", tutorials.findAll);
 
+    router.get("/published", tutorials.findAllPublished);
+
     router.get("/:id", tutorials.findOne);
+
+    router.put("/:id", tutorials.update);
+
+    router.delete("/:id", tutorials.delete);
+
+    router.delete("/", tutorials.deleteAll);
 
     app.use('/api/tutorials', router);
 }
